d703ed8401e43b238138c03ed1e8a400516db477
Facundo Avalos <facundotaraujo@hotmail.com>
2024-09-14 17:33:52 -0300

    modificacion de propiedades

--- vistas/vistaArticulos.Designer.cs
+++ vistas/vistaArticulos.Designer.cs
@@ -29,73 +29,51 @@
         private void InitializeComponent()
         {
             this.dgvArticulos = new System.Windows.Forms.DataGridView();
-            this.picbImagen = new System.Windows.Forms.PictureBox();
-            this.clmDescripción = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.clmCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.clmNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picbImagen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvArticulos
             // 
             this.dgvArticulos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvArticulos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.clmNombre,
-            this.clmCodigo,
-            this.clmDescripción});
             this.dgvArticulos.Location = new System.Drawing.Point(32, 47);
             this.dgvArticulos.Name = "dgvArticulos";
             this.dgvArticulos.Size = new System.Drawing.Size(390, 234);
             this.dgvArticulos.TabIndex = 0;
+            this.dgvArticulos.SelectionChanged += new System.EventHandler(this.dgvArticulos_SelectionChanged);
             // 
-            // picbImagen
+            // pbImagen
             // 
-            this.picbImagen.Location = new System.Drawing.Point(438, 47);
-            this.picbImagen.Name = "picbImagen";
-            this.picbImagen.Size = new System.Drawing.Size(191, 165);
-            this.picbImagen.TabIndex = 1;
-            this.picbImagen.TabStop = false;
-            // 
-            // clmDescripción
-            // 
-            this.clmDescripción.HeaderText = "Descripción";
-            this.clmDescripción.Name = "clmDescripción";
-            // 
-            // clmCodigo
-            // 
-            this.clmCodigo.HeaderText = "Codigo";
-            this.clmCodigo.Name = "clmCodigo";
-            // 
-            // clmNombre
-            // 
-            this.clmNombre.HeaderText = "Nombre";
-            this.clmNombre.Name = "clmNombre";
+            this.pbImagen.Location = new System.Drawing.Point(428, 47);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(191, 234);
+            this.pbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbImagen.TabIndex = 1;
+            this.pbImagen.TabStop = false;
             // 
             // vistaArticulos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.picbImagen);
+            this.Controls.Add(this.pbImagen);
             this.Controls.Add(this.dgvArticulos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "vistaArticulos";
             this.Text = "Catálogo artículos";
             this.Load += new System.EventHandler(this.vistaArticulos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picbImagen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvArticulos;
-        private System.Windows.Forms.PictureBox picbImagen;
-        private System.Windows.Forms.DataGridViewTextBoxColumn clmNombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn clmCodigo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn clmDescripción;
+        private System.Windows.Forms.PictureBox pbImagen;
     }
 }
