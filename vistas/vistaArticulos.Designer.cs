d13e3f8908ca5dd71739cab43e47c114500b73df
Facundo Avalos <facundotaraujo@hotmail.com>
2024-09-14 09:35:48 -0300

    cree una custom tgv

--- vistas/vistaArticulos.Designer.cs
+++ vistas/vistaArticulos.Designer.cs
@@ -29,28 +29,63 @@
         private void InitializeComponent()
         {
             this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            this.picbImagen = new System.Windows.Forms.PictureBox();
+            this.clmDescripción = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbImagen)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvArticulos
             // 
+            this.dgvArticulos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmNombre,
+            this.clmCodigo,
+            this.clmDescripción});
             this.dgvArticulos.Location = new System.Drawing.Point(32, 47);
             this.dgvArticulos.Name = "dgvArticulos";
-            this.dgvArticulos.Size = new System.Drawing.Size(719, 130);
+            this.dgvArticulos.Size = new System.Drawing.Size(390, 234);
             this.dgvArticulos.TabIndex = 0;
+            // 
+            // picbImagen
+            // 
+            this.picbImagen.Location = new System.Drawing.Point(438, 47);
+            this.picbImagen.Name = "picbImagen";
+            this.picbImagen.Size = new System.Drawing.Size(191, 165);
+            this.picbImagen.TabIndex = 1;
+            this.picbImagen.TabStop = false;
+            // 
+            // clmDescripción
+            // 
+            this.clmDescripción.HeaderText = "Descripción";
+            this.clmDescripción.Name = "clmDescripción";
+            // 
+            // clmCodigo
+            // 
+            this.clmCodigo.HeaderText = "Codigo";
+            this.clmCodigo.Name = "clmCodigo";
+            // 
+            // clmNombre
+            // 
+            this.clmNombre.HeaderText = "Nombre";
+            this.clmNombre.Name = "clmNombre";
             // 
             // vistaArticulos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picbImagen);
             this.Controls.Add(this.dgvArticulos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "vistaArticulos";
             this.Text = "Catálogo artículos";
             this.Load += new System.EventHandler(this.vistaArticulos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbImagen)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +93,9 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvArticulos;
+        private System.Windows.Forms.PictureBox picbImagen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmDescripción;
     }
 }
