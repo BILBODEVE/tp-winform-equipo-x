454b999dfcd86af221c7ed4e893f3b66d0f9e1f6
Facundo Avalos <facundotaraujo@hotmail.com>
2024-09-14 17:34:28 -0300

    modificacion de propiedades

--- vistas/vistaArticulos.cs
+++ vistas/vistaArticulos.cs
@@ -1,4 +1,5 @@
 ﻿using negocio;
+using dominio;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,6 +14,7 @@ namespace vistas
 {
     public partial class vistaArticulos : Form
     {
+        private List<Articulo> misArticulos;
         public vistaArticulos()
         {
             InitializeComponent();
@@ -21,7 +23,33 @@ namespace vistas
         private void vistaArticulos_Load(object sender, EventArgs e)
         {
             ArticuloNegocio articuloNegocio = new ArticuloNegocio();
-            dgvArticulos.DataSource = articuloNegocio.listar();
+            misArticulos = articuloNegocio.listar();
+            dgvArticulos.DataSource = misArticulos;
+            dgvArticulos.Columns["Descripcion"].Visible = false;
+            dgvArticulos.Columns["Marca"].Visible = false;
+            dgvArticulos.Columns["Categoria"].Visible = false;
+            dgvArticulos.Columns["Precio"].Visible = false;
+            dgvArticulos.Columns["URLImage"].Visible = false;
+            CargarImagen(misArticulos[0].URLImage);
+        }
+
+        private void dgvArticulos_SelectionChanged(object sender, EventArgs e)
+        {
+            Articulo articuloActual = (Articulo)dgvArticulos.CurrentRow.DataBoundItem; // Devuelve el objeto enlazado de la row
+            CargarImagen(articuloActual.URLImage);
+        }
+
+        private void CargarImagen(string url)
+        {
+            try
+            {
+                pbImagen.Load(url);
+            }
+            catch (Exception ex)
+            {
+                pbImagen.Load("https://placehold.jp/150x150.png");
+                //throw;
+            }
         }
     }
 }
