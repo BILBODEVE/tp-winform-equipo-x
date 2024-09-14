e1724cbf6ef7e57016710342d05dd8cd5171ac59
Facundo Avalos <facundotaraujo@hotmail.com>
2024-09-13 23:07:07 -0300

    botones de navegacion

--- vistas/inicio.Designer.cs
+++ vistas/inicio.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace vistas
 {
-    partial class Form1
+    partial class inicio
     {
         /// <summary>
         /// Required designer variable.
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnVerArticulo = new System.Windows.Forms.Button();
+            this.btnBuscarArticulo = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -36,7 +41,7 @@
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.lblTitulo.Location = new System.Drawing.Point(6F / 2, 34);
+            this.lblTitulo.Location = new System.Drawing.Point(245, 29);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.lblTitulo.Size = new System.Drawing.Size(327, 31);
@@ -44,14 +49,65 @@
             this.lblTitulo.Text = "Administrador de artículos";
             this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // Form1
+            // btnVerArticulo
+            // 
+            this.btnVerArticulo.Location = new System.Drawing.Point(318, 114);
+            this.btnVerArticulo.Name = "btnVerArticulo";
+            this.btnVerArticulo.Size = new System.Drawing.Size(177, 38);
+            this.btnVerArticulo.TabIndex = 1;
+            this.btnVerArticulo.Text = "Ver artículos";
+            this.btnVerArticulo.UseVisualStyleBackColor = true;
+            this.btnVerArticulo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnBuscarArticulo
+            // 
+            this.btnBuscarArticulo.Location = new System.Drawing.Point(318, 158);
+            this.btnBuscarArticulo.Name = "btnBuscarArticulo";
+            this.btnBuscarArticulo.Size = new System.Drawing.Size(177, 38);
+            this.btnBuscarArticulo.TabIndex = 2;
+            this.btnBuscarArticulo.Text = "Buscar";
+            this.btnBuscarArticulo.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(318, 202);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(177, 38);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(318, 246);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(177, 38);
+            this.btnModificar.TabIndex = 4;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(318, 290);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(177, 38);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnBuscarArticulo);
+            this.Controls.Add(this.btnVerArticulo);
             this.Controls.Add(this.lblTitulo);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "inicio";
+            this.Text = "Inicio";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,6 +116,11 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnVerArticulo;
+        private System.Windows.Forms.Button btnBuscarArticulo;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
 
