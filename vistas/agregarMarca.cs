33e07ab3e17e6c10e36f552a5ad72c375dbd2dd4
Facundo Avalos <facundotaraujo@hotmail.com>
2024-09-21 15:59:20 -0300

    Funcionalidad para agregar marca

---
+++ vistas/agregarMarca.cs
@@ -0,0 +1,36 @@
+﻿using negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vistas
+{
+    public partial class agregarMarca : Form
+    {
+        public agregarMarca()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MarcaNegocio nuevaMarca = new MarcaNegocio();
+
+            try
+            {
+                nuevaMarca.Insertar(txtNuevaMarca.Text);
+                MessageBox.Show("La marca " + txtNuevaMarca.Text + " se agrego correctamente!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
