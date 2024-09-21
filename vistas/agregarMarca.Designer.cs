d5bd8e089d10273a8a1355effac8677769e22838
Facundo Avalos <facundotaraujo@hotmail.com>
2024-09-21 15:58:38 -0300

    Implementación de vista para gestion de marcas

---
+++ vistas/agregarMarca.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace vistas
+{
+    partial class agregarMarca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNuevaMarca = new System.Windows.Forms.TextBox();
+            this.btnAgregarMarca = new System.Windows.Forms.Button();
+            this.lblNuevaMarca = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNuevaMarca
+            // 
+            this.txtNuevaMarca.Location = new System.Drawing.Point(80, 54);
+            this.txtNuevaMarca.Name = "txtNuevaMarca";
+            this.txtNuevaMarca.Size = new System.Drawing.Size(121, 20);
+            this.txtNuevaMarca.TabIndex = 0;
+            // 
+            // btnAgregarMarca
+            // 
+            this.btnAgregarMarca.Location = new System.Drawing.Point(80, 80);
+            this.btnAgregarMarca.Name = "btnAgregarMarca";
+            this.btnAgregarMarca.Size = new System.Drawing.Size(123, 30);
+            this.btnAgregarMarca.TabIndex = 1;
+            this.btnAgregarMarca.Text = "Agregar";
+            this.btnAgregarMarca.UseVisualStyleBackColor = true;
+            this.btnAgregarMarca.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblNuevaMarca
+            // 
+            this.lblNuevaMarca.AutoSize = true;
+            this.lblNuevaMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNuevaMarca.Location = new System.Drawing.Point(67, 25);
+            this.lblNuevaMarca.Name = "lblNuevaMarca";
+            this.lblNuevaMarca.Size = new System.Drawing.Size(178, 16);
+            this.lblNuevaMarca.TabIndex = 2;
+            this.lblNuevaMarca.Text = "Ingrese el nombre de marca:";
+            // 
+            // agregarMarca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 156);
+            this.Controls.Add(this.lblNuevaMarca);
+            this.Controls.Add(this.btnAgregarMarca);
+            this.Controls.Add(this.txtNuevaMarca);
+            this.Name = "agregarMarca";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Cargar Marca";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNuevaMarca;
+        private System.Windows.Forms.Button btnAgregarMarca;
+        private System.Windows.Forms.Label lblNuevaMarca;
+    }
+}
