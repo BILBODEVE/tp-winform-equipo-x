98a88ae90d56e13dc47228e294f26d7288f374ce
Facundo Avalos <facundotaraujo@hotmail.com>
2024-09-28 11:28:53 -0300

    Método para validar ingreso de marcas y categorias

---
+++ negocio/HelperNegocio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class HelperNegocio
+    {
+        public bool ValidarNuevoItem(string query, string descripcion)
+        {
+            AccesoDatos consulta = new AccesoDatos();
+
+            try
+            {
+                consulta.SetQuery(query);
+                consulta.Leer();
+                while (consulta.Reader.Read())
+                {
+                    string campo = (string)consulta.Reader["Descripcion"];
+
+                    if (campo.ToUpper() == descripcion.ToUpper()) return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                consulta.Cerrar();
+            }
+            return true;
+        }
+
+        public bool ValidarCampo(string campo)
+        {
+            if(campo == null || campo == "")
+                return false;
+
+            return true;
+        }
+    }
+}
