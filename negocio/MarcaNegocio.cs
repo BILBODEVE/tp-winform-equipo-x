3ace5beeda565d78c23c47b9a7add97fa84216e2
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-02 22:17:14 -0300

    Implemente modificacion + eliminacion fisica + validacion de relacion

--- negocio/MarcaNegocio.cs
+++ negocio/MarcaNegocio.cs
@@ -25,20 +25,20 @@ namespace negocio
                 accesoDatos.Leer();
                 while(accesoDatos.Reader.Read())
                 {
-                   Marca marca = new Marca();
+                    Marca marca = new Marca();
                     marca.Id = (int)accesoDatos.Reader["Id"];
                     marca.Descripcion = (string)accesoDatos.Reader["Descripcion"];
-                   marcas.Add(marca);
+                    marcas.Add(marca);
                 }
 
                 return marcas;
             }
             catch (Exception ex)
-            { 
-                throw ex; 
+            {
+                throw ex;
             }
-            finally 
-            { 
+            finally
+            {
                 accesoDatos.Cerrar();
             }
         }
@@ -48,9 +48,9 @@ namespace negocio
             AccesoDatos datos = new AccesoDatos();
             try
             {
-                datos.SetQuery("INSERT INTO MARCAS(Descripcion)VALUES(@descripcionMarca)");
-                datos.setParametro("@descripcionMarca", descripcionMarca);
-                datos.EjecutarAccion();
+                    datos.SetQuery("INSERT INTO MARCAS(Descripcion)VALUES(@descripcionMarca)");
+                    datos.setParametro("@descripcionMarca", descripcionMarca);
+                    datos.EjecutarAccion();
             }
             catch (Exception ex)
             {
@@ -60,6 +60,70 @@ namespace negocio
             {
                 datos.Cerrar();
             }
+        }
+
+        public void Modificar(Marca marca)
+        {
+            try
+            {
+                accesoDatos.SetQuery("UPDATE MARCAS SET Descripcion = @Descripcion WHERE Id = @Id");
+                accesoDatos.setParametro("@Descripcion", marca.Descripcion);
+                accesoDatos.setParametro("@Id", marca.Id);
+                accesoDatos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.Cerrar();
+            }
+        }
+
+        public void EliminarFisica(Marca marca)
+        {
+            try
+            {
+                accesoDatos.SetQuery("DELETE FROM MARCAS WHERE Id = @Id");
+                accesoDatos.setParametro("@Id", marca.Id);
+                accesoDatos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.Cerrar();
+            }
+        }
+
+        public bool ExisteRelacion(int IDMarca)
+        {
+            try
+            {
+                accesoDatos.SetQuery("SELECT IdMarca FROM ARTICULOS WHERE IdMarca IS NOT NULL");
+                accesoDatos.Leer();
+
+                while (accesoDatos.Reader.Read())
+                {
+                    if (IDMarca == (int)accesoDatos.Reader["IdMarca"])
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.Cerrar();
+            }
+
+            return false;
         }
     }
 }
