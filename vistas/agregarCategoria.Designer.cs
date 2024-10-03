6e66135c992e215406b42a6d47e786053794a004
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-02 22:08:13 -0300

    Nuevo diseño de ventana

--- vistas/agregarCategoria.Designer.cs
+++ vistas/agregarCategoria.Designer.cs
@@ -31,13 +31,24 @@
             this.lblNuevaCategoria = new System.Windows.Forms.Label();
             this.btnAgregarCategoria = new System.Windows.Forms.Button();
             this.txtNuevaCategoria = new System.Windows.Forms.TextBox();
+            this.lblTitleAgregarCategoria = new System.Windows.Forms.Label();
+            this.lblTitleModificarCategoria = new System.Windows.Forms.Label();
+            this.lblSeleccionarCategoria = new System.Windows.Forms.Label();
+            this.cbModificarCategoria = new System.Windows.Forms.ComboBox();
+            this.lblModificarCategoria = new System.Windows.Forms.Label();
+            this.txtModificarCategoria = new System.Windows.Forms.TextBox();
+            this.btnModificarCategoria = new System.Windows.Forms.Button();
+            this.lblTitleEliminarCategoria = new System.Windows.Forms.Label();
+            this.lblEliminarCategoria = new System.Windows.Forms.Label();
+            this.cbEliminarCategoria = new System.Windows.Forms.ComboBox();
+            this.btnEliminarCategoria = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblNuevaCategoria
             // 
             this.lblNuevaCategoria.AutoSize = true;
             this.lblNuevaCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNuevaCategoria.Location = new System.Drawing.Point(60, 24);
+            this.lblNuevaCategoria.Location = new System.Drawing.Point(43, 56);
             this.lblNuevaCategoria.Name = "lblNuevaCategoria";
             this.lblNuevaCategoria.Size = new System.Drawing.Size(197, 16);
             this.lblNuevaCategoria.TabIndex = 5;
@@ -45,9 +56,9 @@
             // 
             // btnAgregarCategoria
             // 
-            this.btnAgregarCategoria.Location = new System.Drawing.Point(83, 79);
+            this.btnAgregarCategoria.Location = new System.Drawing.Point(373, 52);
             this.btnAgregarCategoria.Name = "btnAgregarCategoria";
-            this.btnAgregarCategoria.Size = new System.Drawing.Size(123, 30);
+            this.btnAgregarCategoria.Size = new System.Drawing.Size(87, 25);
             this.btnAgregarCategoria.TabIndex = 4;
             this.btnAgregarCategoria.Text = "Agregar";
             this.btnAgregarCategoria.UseVisualStyleBackColor = true;
@@ -55,22 +66,137 @@
             // 
             // txtNuevaCategoria
             // 
-            this.txtNuevaCategoria.Location = new System.Drawing.Point(83, 53);
+            this.txtNuevaCategoria.Location = new System.Drawing.Point(246, 55);
             this.txtNuevaCategoria.Name = "txtNuevaCategoria";
             this.txtNuevaCategoria.Size = new System.Drawing.Size(121, 20);
             this.txtNuevaCategoria.TabIndex = 3;
+            // 
+            // lblTitleAgregarCategoria
+            // 
+            this.lblTitleAgregarCategoria.AutoSize = true;
+            this.lblTitleAgregarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleAgregarCategoria.Location = new System.Drawing.Point(12, 28);
+            this.lblTitleAgregarCategoria.Name = "lblTitleAgregarCategoria";
+            this.lblTitleAgregarCategoria.Size = new System.Drawing.Size(170, 16);
+            this.lblTitleAgregarCategoria.TabIndex = 6;
+            this.lblTitleAgregarCategoria.Text = "Cargar nueva categoria";
+            // 
+            // lblTitleModificarCategoria
+            // 
+            this.lblTitleModificarCategoria.AutoSize = true;
+            this.lblTitleModificarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleModificarCategoria.Location = new System.Drawing.Point(12, 104);
+            this.lblTitleModificarCategoria.Name = "lblTitleModificarCategoria";
+            this.lblTitleModificarCategoria.Size = new System.Drawing.Size(141, 16);
+            this.lblTitleModificarCategoria.TabIndex = 7;
+            this.lblTitleModificarCategoria.Text = "Modificar categoría";
+            // 
+            // lblSeleccionarCategoria
+            // 
+            this.lblSeleccionarCategoria.AutoSize = true;
+            this.lblSeleccionarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeleccionarCategoria.Location = new System.Drawing.Point(43, 138);
+            this.lblSeleccionarCategoria.Name = "lblSeleccionarCategoria";
+            this.lblSeleccionarCategoria.Size = new System.Drawing.Size(152, 16);
+            this.lblSeleccionarCategoria.TabIndex = 8;
+            this.lblSeleccionarCategoria.Text = "Seleccione la categoría:";
+            // 
+            // cbModificarCategoria
+            // 
+            this.cbModificarCategoria.FormattingEnabled = true;
+            this.cbModificarCategoria.Location = new System.Drawing.Point(246, 137);
+            this.cbModificarCategoria.Name = "cbModificarCategoria";
+            this.cbModificarCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cbModificarCategoria.TabIndex = 9;
+            // 
+            // lblModificarCategoria
+            // 
+            this.lblModificarCategoria.AutoSize = true;
+            this.lblModificarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModificarCategoria.Location = new System.Drawing.Point(43, 171);
+            this.lblModificarCategoria.Name = "lblModificarCategoria";
+            this.lblModificarCategoria.Size = new System.Drawing.Size(197, 16);
+            this.lblModificarCategoria.TabIndex = 10;
+            this.lblModificarCategoria.Text = "Ingrese el nombre de categoria:";
+            // 
+            // txtModificarCategoria
+            // 
+            this.txtModificarCategoria.Location = new System.Drawing.Point(246, 171);
+            this.txtModificarCategoria.Name = "txtModificarCategoria";
+            this.txtModificarCategoria.Size = new System.Drawing.Size(121, 20);
+            this.txtModificarCategoria.TabIndex = 11;
+            // 
+            // btnModificarCategoria
+            // 
+            this.btnModificarCategoria.Location = new System.Drawing.Point(373, 166);
+            this.btnModificarCategoria.Name = "btnModificarCategoria";
+            this.btnModificarCategoria.Size = new System.Drawing.Size(87, 25);
+            this.btnModificarCategoria.TabIndex = 12;
+            this.btnModificarCategoria.Text = "Modificar";
+            this.btnModificarCategoria.UseVisualStyleBackColor = true;
+            this.btnModificarCategoria.Click += new System.EventHandler(this.btnModificarCategoria_Click);
+            // 
+            // lblTitleEliminarCategoria
+            // 
+            this.lblTitleEliminarCategoria.AutoSize = true;
+            this.lblTitleEliminarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleEliminarCategoria.Location = new System.Drawing.Point(12, 206);
+            this.lblTitleEliminarCategoria.Name = "lblTitleEliminarCategoria";
+            this.lblTitleEliminarCategoria.Size = new System.Drawing.Size(133, 16);
+            this.lblTitleEliminarCategoria.TabIndex = 13;
+            this.lblTitleEliminarCategoria.Text = "Eliminar categoría";
+            // 
+            // lblEliminarCategoria
+            // 
+            this.lblEliminarCategoria.AutoSize = true;
+            this.lblEliminarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEliminarCategoria.Location = new System.Drawing.Point(43, 240);
+            this.lblEliminarCategoria.Name = "lblEliminarCategoria";
+            this.lblEliminarCategoria.Size = new System.Drawing.Size(152, 16);
+            this.lblEliminarCategoria.TabIndex = 14;
+            this.lblEliminarCategoria.Text = "Seleccione la categoría:";
+            // 
+            // cbEliminarCategoria
+            // 
+            this.cbEliminarCategoria.FormattingEnabled = true;
+            this.cbEliminarCategoria.Location = new System.Drawing.Point(246, 239);
+            this.cbEliminarCategoria.Name = "cbEliminarCategoria";
+            this.cbEliminarCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cbEliminarCategoria.TabIndex = 15;
+            // 
+            // btnEliminarCategoria
+            // 
+            this.btnEliminarCategoria.Location = new System.Drawing.Point(373, 236);
+            this.btnEliminarCategoria.Name = "btnEliminarCategoria";
+            this.btnEliminarCategoria.Size = new System.Drawing.Size(87, 25);
+            this.btnEliminarCategoria.TabIndex = 16;
+            this.btnEliminarCategoria.Text = "Eliminar";
+            this.btnEliminarCategoria.UseVisualStyleBackColor = true;
+            this.btnEliminarCategoria.Click += new System.EventHandler(this.btnEliminarCategoria_Click);
             // 
             // agregarCategoria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 156);
+            this.ClientSize = new System.Drawing.Size(523, 298);
+            this.Controls.Add(this.btnEliminarCategoria);
+            this.Controls.Add(this.cbEliminarCategoria);
+            this.Controls.Add(this.lblEliminarCategoria);
+            this.Controls.Add(this.lblTitleEliminarCategoria);
+            this.Controls.Add(this.btnModificarCategoria);
+            this.Controls.Add(this.txtModificarCategoria);
+            this.Controls.Add(this.lblModificarCategoria);
+            this.Controls.Add(this.cbModificarCategoria);
+            this.Controls.Add(this.lblSeleccionarCategoria);
+            this.Controls.Add(this.lblTitleModificarCategoria);
+            this.Controls.Add(this.lblTitleAgregarCategoria);
             this.Controls.Add(this.lblNuevaCategoria);
             this.Controls.Add(this.btnAgregarCategoria);
             this.Controls.Add(this.txtNuevaCategoria);
             this.Name = "agregarCategoria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "agregarCategoria";
+            this.Load += new System.EventHandler(this.agregarCategoria_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,5 +207,16 @@
         private System.Windows.Forms.Label lblNuevaCategoria;
         private System.Windows.Forms.Button btnAgregarCategoria;
         private System.Windows.Forms.TextBox txtNuevaCategoria;
+        private System.Windows.Forms.Label lblTitleAgregarCategoria;
+        private System.Windows.Forms.Label lblTitleModificarCategoria;
+        private System.Windows.Forms.Label lblSeleccionarCategoria;
+        private System.Windows.Forms.ComboBox cbModificarCategoria;
+        private System.Windows.Forms.Label lblModificarCategoria;
+        private System.Windows.Forms.TextBox txtModificarCategoria;
+        private System.Windows.Forms.Button btnModificarCategoria;
+        private System.Windows.Forms.Label lblTitleEliminarCategoria;
+        private System.Windows.Forms.Label lblEliminarCategoria;
+        private System.Windows.Forms.ComboBox cbEliminarCategoria;
+        private System.Windows.Forms.Button btnEliminarCategoria;
     }
 }
