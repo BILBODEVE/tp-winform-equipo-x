9f8584b03e990348537bd0478e2f2eb2844c0a0c
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-02 22:08:31 -0300

    Nuevo diseño de ventana

--- vistas/agregarMarca.Designer.cs
+++ vistas/agregarMarca.Designer.cs
@@ -28,58 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNuevaMarca = new System.Windows.Forms.TextBox();
-            this.btnAgregarMarca = new System.Windows.Forms.Button();
+            this.btnEliminarMarca = new System.Windows.Forms.Button();
+            this.cbEliminarMarca = new System.Windows.Forms.ComboBox();
+            this.lblEliminarMarca = new System.Windows.Forms.Label();
+            this.lblTitleEliminarMarca = new System.Windows.Forms.Label();
+            this.btnModificarMarca = new System.Windows.Forms.Button();
+            this.txtModificarMarca = new System.Windows.Forms.TextBox();
+            this.lblModificarMarca = new System.Windows.Forms.Label();
+            this.cbModificarMarca = new System.Windows.Forms.ComboBox();
+            this.lblSeleccionarMarca = new System.Windows.Forms.Label();
+            this.lblTitleModificarMarca = new System.Windows.Forms.Label();
+            this.lblTitleAgregarMarca = new System.Windows.Forms.Label();
             this.lblNuevaMarca = new System.Windows.Forms.Label();
+            this.btnAgregarMarca = new System.Windows.Forms.Button();
+            this.txtNuevaMarca = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // txtNuevaMarca
+            // btnEliminarMarca
             // 
-            this.txtNuevaMarca.Location = new System.Drawing.Point(80, 54);
-            this.txtNuevaMarca.Name = "txtNuevaMarca";
-            this.txtNuevaMarca.Size = new System.Drawing.Size(121, 20);
-            this.txtNuevaMarca.TabIndex = 0;
+            this.btnEliminarMarca.Location = new System.Drawing.Point(398, 241);
+            this.btnEliminarMarca.Name = "btnEliminarMarca";
+            this.btnEliminarMarca.Size = new System.Drawing.Size(87, 25);
+            this.btnEliminarMarca.TabIndex = 30;
+            this.btnEliminarMarca.Text = "Eliminar";
+            this.btnEliminarMarca.UseVisualStyleBackColor = true;
+            this.btnEliminarMarca.Click += new System.EventHandler(this.btnEliminarMarca_Click);
             // 
-            // btnAgregarMarca
+            // cbEliminarMarca
             // 
-            this.btnAgregarMarca.Location = new System.Drawing.Point(80, 80);
-            this.btnAgregarMarca.Name = "btnAgregarMarca";
-            this.btnAgregarMarca.Size = new System.Drawing.Size(123, 30);
-            this.btnAgregarMarca.TabIndex = 1;
-            this.btnAgregarMarca.Text = "Agregar";
-            this.btnAgregarMarca.UseVisualStyleBackColor = true;
-            this.btnAgregarMarca.Click += new System.EventHandler(this.button1_Click);
+            this.cbEliminarMarca.FormattingEnabled = true;
+            this.cbEliminarMarca.Location = new System.Drawing.Point(271, 244);
+            this.cbEliminarMarca.Name = "cbEliminarMarca";
+            this.cbEliminarMarca.Size = new System.Drawing.Size(121, 21);
+            this.cbEliminarMarca.TabIndex = 29;
+            // 
+            // lblEliminarMarca
+            // 
+            this.lblEliminarMarca.AutoSize = true;
+            this.lblEliminarMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEliminarMarca.Location = new System.Drawing.Point(68, 245);
+            this.lblEliminarMarca.Name = "lblEliminarMarca";
+            this.lblEliminarMarca.Size = new System.Drawing.Size(152, 16);
+            this.lblEliminarMarca.TabIndex = 28;
+            this.lblEliminarMarca.Text = "Seleccione la categoría:";
+            // 
+            // lblTitleEliminarMarca
+            // 
+            this.lblTitleEliminarMarca.AutoSize = true;
+            this.lblTitleEliminarMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleEliminarMarca.Location = new System.Drawing.Point(37, 211);
+            this.lblTitleEliminarMarca.Name = "lblTitleEliminarMarca";
+            this.lblTitleEliminarMarca.Size = new System.Drawing.Size(110, 16);
+            this.lblTitleEliminarMarca.TabIndex = 27;
+            this.lblTitleEliminarMarca.Text = "Eliminar marca";
+            // 
+            // btnModificarMarca
+            // 
+            this.btnModificarMarca.Location = new System.Drawing.Point(398, 171);
+            this.btnModificarMarca.Name = "btnModificarMarca";
+            this.btnModificarMarca.Size = new System.Drawing.Size(87, 25);
+            this.btnModificarMarca.TabIndex = 26;
+            this.btnModificarMarca.Text = "Modificar";
+            this.btnModificarMarca.UseVisualStyleBackColor = true;
+            this.btnModificarMarca.Click += new System.EventHandler(this.btnModificarMarca_Click);
+            // 
+            // txtModificarMarca
+            // 
+            this.txtModificarMarca.Location = new System.Drawing.Point(271, 176);
+            this.txtModificarMarca.Name = "txtModificarMarca";
+            this.txtModificarMarca.Size = new System.Drawing.Size(121, 20);
+            this.txtModificarMarca.TabIndex = 25;
+            // 
+            // lblModificarMarca
+            // 
+            this.lblModificarMarca.AutoSize = true;
+            this.lblModificarMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModificarMarca.Location = new System.Drawing.Point(68, 176);
+            this.lblModificarMarca.Name = "lblModificarMarca";
+            this.lblModificarMarca.Size = new System.Drawing.Size(178, 16);
+            this.lblModificarMarca.TabIndex = 24;
+            this.lblModificarMarca.Text = "Ingrese el nombre de marca:";
+            // 
+            // cbModificarMarca
+            // 
+            this.cbModificarMarca.FormattingEnabled = true;
+            this.cbModificarMarca.Location = new System.Drawing.Point(271, 142);
+            this.cbModificarMarca.Name = "cbModificarMarca";
+            this.cbModificarMarca.Size = new System.Drawing.Size(121, 21);
+            this.cbModificarMarca.TabIndex = 23;
+            // 
+            // lblSeleccionarMarca
+            // 
+            this.lblSeleccionarMarca.AutoSize = true;
+            this.lblSeleccionarMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeleccionarMarca.Location = new System.Drawing.Point(68, 143);
+            this.lblSeleccionarMarca.Name = "lblSeleccionarMarca";
+            this.lblSeleccionarMarca.Size = new System.Drawing.Size(133, 16);
+            this.lblSeleccionarMarca.TabIndex = 22;
+            this.lblSeleccionarMarca.Text = "Seleccione la marca:";
+            // 
+            // lblTitleModificarMarca
+            // 
+            this.lblTitleModificarMarca.AutoSize = true;
+            this.lblTitleModificarMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleModificarMarca.Location = new System.Drawing.Point(37, 109);
+            this.lblTitleModificarMarca.Name = "lblTitleModificarMarca";
+            this.lblTitleModificarMarca.Size = new System.Drawing.Size(118, 16);
+            this.lblTitleModificarMarca.TabIndex = 21;
+            this.lblTitleModificarMarca.Text = "Modificar marca";
+            // 
+            // lblTitleAgregarMarca
+            // 
+            this.lblTitleAgregarMarca.AutoSize = true;
+            this.lblTitleAgregarMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleAgregarMarca.Location = new System.Drawing.Point(37, 33);
+            this.lblTitleAgregarMarca.Name = "lblTitleAgregarMarca";
+            this.lblTitleAgregarMarca.Size = new System.Drawing.Size(147, 16);
+            this.lblTitleAgregarMarca.TabIndex = 20;
+            this.lblTitleAgregarMarca.Text = "Cargar nueva marca";
             // 
             // lblNuevaMarca
             // 
             this.lblNuevaMarca.AutoSize = true;
             this.lblNuevaMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNuevaMarca.Location = new System.Drawing.Point(63, 24);
+            this.lblNuevaMarca.Location = new System.Drawing.Point(68, 61);
             this.lblNuevaMarca.Name = "lblNuevaMarca";
             this.lblNuevaMarca.Size = new System.Drawing.Size(178, 16);
-            this.lblNuevaMarca.TabIndex = 2;
+            this.lblNuevaMarca.TabIndex = 19;
             this.lblNuevaMarca.Text = "Ingrese el nombre de marca:";
+            // 
+            // btnAgregarMarca
+            // 
+            this.btnAgregarMarca.Location = new System.Drawing.Point(398, 57);
+            this.btnAgregarMarca.Name = "btnAgregarMarca";
+            this.btnAgregarMarca.Size = new System.Drawing.Size(87, 25);
+            this.btnAgregarMarca.TabIndex = 18;
+            this.btnAgregarMarca.Text = "Agregar";
+            this.btnAgregarMarca.UseVisualStyleBackColor = true;
+            // 
+            // txtNuevaMarca
+            // 
+            this.txtNuevaMarca.Location = new System.Drawing.Point(271, 60);
+            this.txtNuevaMarca.Name = "txtNuevaMarca";
+            this.txtNuevaMarca.Size = new System.Drawing.Size(121, 20);
+            this.txtNuevaMarca.TabIndex = 17;
             // 
             // agregarMarca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 156);
+            this.ClientSize = new System.Drawing.Size(523, 298);
+            this.Controls.Add(this.btnEliminarMarca);
+            this.Controls.Add(this.cbEliminarMarca);
+            this.Controls.Add(this.lblEliminarMarca);
+            this.Controls.Add(this.lblTitleEliminarMarca);
+            this.Controls.Add(this.btnModificarMarca);
+            this.Controls.Add(this.txtModificarMarca);
+            this.Controls.Add(this.lblModificarMarca);
+            this.Controls.Add(this.cbModificarMarca);
+            this.Controls.Add(this.lblSeleccionarMarca);
+            this.Controls.Add(this.lblTitleModificarMarca);
+            this.Controls.Add(this.lblTitleAgregarMarca);
             this.Controls.Add(this.lblNuevaMarca);
             this.Controls.Add(this.btnAgregarMarca);
             this.Controls.Add(this.txtNuevaMarca);
             this.Name = "agregarMarca";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Cargar Marca";
+            this.Load += new System.EventHandler(this.agregarMarca_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtNuevaMarca;
-        private System.Windows.Forms.Button btnAgregarMarca;
+        private System.Windows.Forms.Button btnEliminarMarca;
+        private System.Windows.Forms.ComboBox cbEliminarMarca;
+        private System.Windows.Forms.Label lblEliminarMarca;
+        private System.Windows.Forms.Label lblTitleEliminarMarca;
+        private System.Windows.Forms.Button btnModificarMarca;
+        private System.Windows.Forms.TextBox txtModificarMarca;
+        private System.Windows.Forms.Label lblModificarMarca;
+        private System.Windows.Forms.ComboBox cbModificarMarca;
+        private System.Windows.Forms.Label lblSeleccionarMarca;
+        private System.Windows.Forms.Label lblTitleModificarMarca;
+        private System.Windows.Forms.Label lblTitleAgregarMarca;
         private System.Windows.Forms.Label lblNuevaMarca;
+        private System.Windows.Forms.Button btnAgregarMarca;
+        private System.Windows.Forms.TextBox txtNuevaMarca;
     }
 }
