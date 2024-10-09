ad62c0c405b33079e4d81922a8594811f5d357ee
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-09 19:10:51 -0300

    Validación campos null + vacios + numéricos

--- vistas/agregarArticulo.cs
+++ vistas/agregarArticulo.cs
@@ -9,16 +9,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace vistas
 {
     public partial class agregarArticulo : Form
     {
         private Articulo articulo = null;
-        
+        private HelperNegocio helper;
+
         public agregarArticulo()
         {
             InitializeComponent();
+            helper = new HelperNegocio();
         }
         public agregarArticulo(Articulo articulo)
         {
@@ -36,24 +39,27 @@ namespace vistas
             ArticuloNegocio nuevoArticulo = new ArticuloNegocio();
             ImagenNegocio imagenNegocio = new ImagenNegocio();
 
-            if(articulo == null)
+            if (articulo == null)
                 articulo = new Articulo();
 
-            articulo.Codigo = txtCodigo.Text;
-            articulo.Nombre = txtNombre.Text;
-            articulo.Descripcion = txtDescripcion.Text;
-            articulo.Marca = (Marca)dpdMarca.SelectedItem;
-            articulo.Categoria = (Categoria)dpdCategoria.SelectedItem;
-            articulo.Imagen[0] = txtUrlImagen.Text;
-            articulo.Precio = decimal.Parse(txtPrecio.Text);
+            if (ValidarCamposRequeridos())
+            {
+                articulo.Codigo = txtCodigo.Text;
+                articulo.Nombre = txtNombre.Text;
+                articulo.Descripcion = txtDescripcion.Text;
+                articulo.Marca = (Marca)dpdMarca.SelectedItem;
+                articulo.Categoria = (Categoria)dpdCategoria.SelectedItem;
+                articulo.Imagen.Add(txtUrlImagen.Text);
+                articulo.Precio = decimal.Parse(txtPrecio.Text);
 
-            if(articulo.Id != 0)
-                nuevoArticulo.Modificar(articulo);
-            else
-                nuevoArticulo.Cargar(articulo);
+                if (articulo.Id != 0)
+                    nuevoArticulo.Modificar(articulo);
+                else
+                    nuevoArticulo.Cargar(articulo);
 
-            MessageBox.Show("Operacion exitosa");
-            LimpiarCampos();
+                MessageBox.Show("Operacion exitosa");
+                LimpiarCampos();
+            }
         }
         private void agregarArticulo_Load(object sender, EventArgs e)
         {
@@ -84,7 +90,6 @@ namespace vistas
                 throw ex;
             }
         }
-
         private void LimpiarCampos()
         {
             txtCodigo.Text = null;
@@ -92,6 +97,49 @@ namespace vistas
             txtDescripcion.Text = null;
             txtUrlImagen.Text = null;
             txtPrecio.Text = null;
+        }
+
+        private bool ValidarCampoNulo(System.Windows.Forms.TextBox TextBox)
+        {
+            if (string.IsNullOrEmpty(TextBox.Text))
+            {
+                errorProviderCodigo.SetError(TextBox, "El campo requiere un valor");
+                return false;
+            }
+            else
+            {
+                errorProviderCodigo.SetError(TextBox, "");
+                return true;
+            }
+        }
+        private bool ValidarCampoNumerico(System.Windows.Forms.TextBox TextBox)
+        {
+            try
+            {
+                if (decimal.Parse(txtPrecio.Text) < 0)
+                {
+                    errorProviderCodigo.SetError(TextBox, "El precio no puede ser negativo");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                errorProviderCodigo.SetError(txtPrecio, "Ingrese un valor númerico adecuado");// Manejo de System.FormatException
+            }
+            return true;
+        }
+        private bool ValidarCamposRequeridos()
+        {
+            if (!ValidarCampoNulo(txtCodigo))
+                return false;
+            if (!ValidarCampoNulo(txtNombre))
+                return false;
+            if (!ValidarCampoNulo(txtUrlImagen))
+                return false;
+            if (!ValidarCampoNulo(txtPrecio) || !ValidarCampoNumerico(txtPrecio))
+                return false;
+
+            return true;
         }
     }
 }
